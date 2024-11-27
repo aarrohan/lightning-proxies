9916262b9d6a09ad12c263ccfd9a7167d7b08f15
Abdullah Al Reza Rohan <developer.aarrohan@gmail.com>
2024-11-27 13:48:38 +0600

    feat(ui): add ad verification page

---
+++ src/app/use-cases/ad-verification/DetailsSection.tsx
@@ -0,0 +1,331 @@
+import Image from "next/image";
+import detailsImg from "@/assets/images/ad-verification-details.svg";
+
+interface IInfoCardProps {
+  icon: JSX.Element;
+  title: string;
+  description: string;
+}
+
+function InfoCard({ icon, title, description }: IInfoCardProps) {
+  return (
+    <div>
+      <div className="mb-2 flex items-center gap-2 sm:gap-2.5">
+        {icon}
+
+        <h3 className="text-xs sm:text-base font-medium tracking-[-0.16px] text-[#A400DD]">
+          {title}
+        </h3>
+      </div>
+
+      <p className="text-[11px] sm:text-base tracking-[-0.11px] sm:tracking-[-0.16px] text-primary">
+        {description}
+      </p>
+    </div>
+  );
+}
+
+export default function DetailsSection() {
+  const infoCards: IInfoCardProps[] = [
+    {
+      icon: (
+        <svg
+          width="24"
+          height="25"
+          viewBox="0 0 24 25"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          className="w-[18px] sm:w-[24px] h-auto"
+        >
+          <path
+            d="M22 12.0352C22 6.51231 17.5228 2.03516 12 2.03516C6.47715 2.03516 2 6.51231 2 12.0352C2 17.558 6.47715 22.0352 12 22.0352"
+            stroke="#A400DD"
+            strokeWidth="1.5"
+            strokeLinecap="round"
+          />
+          <path
+            d="M20 5.73415C19.0653 5.80152 17.8681 6.1634 17.0379 7.23793C15.5385 9.17877 14.039 9.34072 13.0394 8.69377C11.5399 7.72336 12.8 6.15152 11.0401 5.29731C9.89313 4.74058 9.73321 3.22561 10.3716 2.03516"
+            stroke="#A400DD"
+            strokeWidth="1.5"
+            strokeLinejoin="round"
+          />
+          <path
+            d="M2 11.0352C2.7625 11.6973 3.83046 12.3034 5.08874 12.3034C7.68843 12.3034 8.20837 12.8001 8.20837 14.787C8.20837 16.7739 8.20837 16.7739 8.72831 18.264C9.06651 19.2333 9.18472 20.2026 8.5106 21.0352"
+            stroke="#A400DD"
+            strokeWidth="1.5"
+            strokeLinejoin="round"
+          />
+          <path
+            d="M19.8988 19.964L22 22.0352M21.1083 17.0811C21.1083 19.3157 19.2932 21.1271 17.0541 21.1271C14.8151 21.1271 13 19.3157 13 17.0811C13 14.8466 14.8151 13.0352 17.0541 13.0352C19.2932 13.0352 21.1083 14.8466 21.1083 17.0811Z"
+            stroke="#A400DD"
+            strokeWidth="1.5"
+            strokeLinecap="round"
+          />
+        </svg>
+      ),
+      title: "Reliable Infrastructure",
+      description:
+        "Ethically sourced IPs and top-quality data center partners globally.",
+    },
+    {
+      icon: (
+        <svg
+          width="24"
+          height="25"
+          viewBox="0 0 24 25"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          className="w-[18px] sm:w-[24px] h-auto"
+        >
+          <path
+            d="M19 20.5352C19.4255 20.3442 19.7894 20.0964 20.1088 19.7747C21.5 18.3731 21.5 16.1173 21.5 11.6057C21.5 7.0941 21.5 4.8383 20.1088 3.43673C18.7175 2.03516 16.4783 2.03516 12 2.03516C7.52166 2.03516 5.28249 2.03516 3.89124 3.43673C2.5 4.8383 2.5 7.0941 2.5 11.6057C2.5 16.1173 2.5 18.3731 3.89124 19.7747C4.21056 20.0964 4.57453 20.3442 5 20.5352"
+            stroke="#A400DD"
+            strokeWidth="1.5"
+            strokeLinecap="round"
+          />
+          <path
+            d="M2.5 8.53516H21.5"
+            stroke="#A400DD"
+            strokeWidth="1.5"
+            strokeLinejoin="round"
+          />
+          <path
+            d="M7 5.53516H7.00898"
+            stroke="#A400DD"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
+          <path
+            d="M11 5.53516H11.009"
+            stroke="#A400DD"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
+          <path
+            d="M10.2596 16.4131C9.1796 16.4131 8.71714 17.1926 8.59714 17.6724C8.47714 18.1521 8.47714 19.8911 8.54914 20.6106C8.78914 21.5101 9.38914 21.8819 9.97714 22.0018C10.5171 22.0498 12.7971 22.0318 13.4571 22.0318C14.4171 22.0498 15.1371 21.69 15.4371 20.6106C15.4971 20.2508 15.5571 18.272 15.4071 17.6724C15.0891 16.7129 14.36 16.4131 13.76 16.4131M10.2596 16.4131H13.76M10.2596 16.4131C10.2596 16.3531 10.2582 15.5867 10.2596 15.1524C10.2609 14.7555 10.226 14.3729 10.4156 14.0227C11.126 12.6099 13.166 12.7538 13.67 14.193C13.7573 14.4299 13.7626 14.8055 13.76 15.1524C13.7567 15.5956 13.76 16.4131 13.76 16.4131"
+            stroke="#A400DD"
+            strokeWidth="1.5"
+            strokeLinecap="round"
+          />
+        </svg>
+      ),
+      title: "Geo-location targeting",
+      description:
+        "Choose from 180+ locations worldwide, by city, state and ASN.",
+    },
+    {
+      icon: (
+        <svg
+          width="24"
+          height="25"
+          viewBox="0 0 24 25"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          className="w-[18px] sm:w-[24px] h-auto"
+        >
+          <path
+            d="M15 2.49296C14.053 2.19551 13.0452 2.03516 12 2.03516C6.47715 2.03516 2 6.51231 2 12.0352C2 17.558 6.47715 22.0352 12 22.0352C17.5228 22.0352 22 17.558 22 12.0352C22 10.99 21.8396 9.9822 21.5422 9.03516"
+            stroke="#A400DD"
+            strokeWidth="1.5"
+            strokeLinecap="round"
+          />
+          <path
+            d="M8.5 9.53516L12 13.0352L21.0002 3.03516"
+            stroke="#A400DD"
+            strokeWidth="1.5"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
+        </svg>
+      ),
+      title: "High succes rates",
+      description:
+        "Our Residential proxy network ensures success rates of up to 99%.",
+    },
+    {
+      icon: (
+        <svg
+          width="24"
+          height="25"
+          viewBox="0 0 24 25"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          className="w-[18px] sm:w-[24px] h-auto"
+        >
+          <path
+            d="M2.5 12.0352C2.5 7.55682 2.5 5.31765 3.89124 3.9264C5.28249 2.53516 7.52166 2.53516 12 2.53516C16.4783 2.53516 18.7175 2.53516 20.1088 3.9264C21.5 5.31765 21.5 7.55682 21.5 12.0352C21.5 16.5135 21.5 18.7527 20.1088 20.144C18.7175 21.5352 16.4783 21.5352 12 21.5352C7.52166 21.5352 5.28249 21.5352 3.89124 20.144C2.5 18.7527 2.5 16.5135 2.5 12.0352Z"
+            stroke="#A400DD"
+            strokeWidth="1.5"
+          />
+          <path
+            d="M16 16.0352C16.6161 16.0352 17.1155 15.5235 17.1155 14.8923V13.6514C17.1155 13.3483 17.233 13.0576 17.4422 12.8433L17.8366 12.4393C18.0545 12.2161 18.0545 11.8543 17.8366 11.6311L17.4422 11.2271C17.233 11.0128 17.1155 10.7221 17.1155 10.419V9.17802C17.1155 8.54683 16.6161 8.03516 16 8.03516"
+            stroke="#A400DD"
+            strokeWidth="1.5"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
+          <path
+            d="M8 16.0352C7.38392 16.0352 6.88448 15.5235 6.88448 14.8923V13.6514C6.88448 13.3483 6.76696 13.0576 6.55776 12.8433L6.16336 12.4393C5.94555 12.2161 5.94555 11.8543 6.16336 11.6311L6.55776 11.2271C6.76696 11.0128 6.88448 10.7221 6.88448 10.419V9.17802C6.88448 8.54683 7.38392 8.03516 8 8.03516"
+            stroke="#A400DD"
+            strokeWidth="1.5"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
+          <path
+            d="M10 12.0352H10.009M13.991 12.0352H14"
+            stroke="#A400DD"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
+        </svg>
+      ),
+      title: "Multiple protocols",
+      description:
+        "We provide proxy support for HTTP, HTTPS and SOCKS5 protocols.",
+    },
+  ];
+
+  return (
+    <section className="sm:pt-44 pb-20 sm:pb-44 bg-gradient-to-b from-transparent to-off-white">
+      <div className="mx-auto container max-w-[1320px] px-5">
+        <div className="grid lg:grid-cols-2 gap-y-10 gap-x-16 items-center">
+          <div className="order-2 sm:order-1 flex">
+            <Image
+              src={detailsImg}
+              alt=""
+              className="w-full border border-dark-white rounded-xl"
+            />
+          </div>
+
+          <div className="order-1 sm:order-2">
+            <p className="mb-3 text-[11px] sm:text-sm font-semibold tracking-[-0.11px] sm:tracking-[-0.14px] uppercase text-[#A400DD]">
+              AD VERIFICATION
+            </p>
+
+            <h2 className="mb-6 text-[26px] sm:text-[42px] font-semibold leading-[30px] sm:leading-[46px] tracking-[-0.84px]">
+              Get your ads verified using our reliable proxy solutions
+            </h2>
+
+            <p className="mb-6 text-xs sm:text-base tracking-[-0.12px] sm:tracking-[-0.16px] text-primary/75">
+              In ullamcorper diam neque, commodo eleifend nisl imperdiet sed.
+              Pellentesque sollicitudin malesuada lorem porta sagittis. Quisque
+              id tortor quis diam porttitor euismod. Phasellus et mauris nulla.
+            </p>
+
+            <div className="mb-8 space-y-4">
+              <div className="flex items-center gap-2 sm:gap-2.5">
+                <svg
+                  width="24"
+                  height="25"
+                  viewBox="0 0 24 25"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="w-[16px] sm:w-[24px] h-auto"
+                >
+                  <path
+                    d="M22 12.0352C22 6.51231 17.5228 2.03516 12 2.03516C6.47715 2.03516 2 6.51231 2 12.0352C2 17.558 6.47715 22.0352 12 22.0352C17.5228 22.0352 22 17.558 22 12.0352Z"
+                    stroke="#A400DD"
+                    strokeWidth="1.5"
+                  />
+                  <path
+                    d="M8 12.5352L10.5 15.0352L16 9.03516"
+                    stroke="#A400DD"
+                    strokeWidth="1.5"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                </svg>
+
+                <p className="text-xs sm:text-base tracking-[-0.12px] sm:tracking-[-0.16px] text-primary/75">
+                  In diam neque, commodo eleifend nisl imperdiet sed.
+                </p>
+              </div>
+
+              <div className="flex items-center gap-2 sm:gap-2.5">
+                <svg
+                  width="24"
+                  height="25"
+                  viewBox="0 0 24 25"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="w-[16px] sm:w-[24px] h-auto"
+                >
+                  <path
+                    d="M22 12.0352C22 6.51231 17.5228 2.03516 12 2.03516C6.47715 2.03516 2 6.51231 2 12.0352C2 17.558 6.47715 22.0352 12 22.0352C17.5228 22.0352 22 17.558 22 12.0352Z"
+                    stroke="#A400DD"
+                    strokeWidth="1.5"
+                  />
+                  <path
+                    d="M8 12.5352L10.5 15.0352L16 9.03516"
+                    stroke="#A400DD"
+                    strokeWidth="1.5"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                </svg>
+
+                <p className="text-xs sm:text-base tracking-[-0.12px] sm:tracking-[-0.16px] text-primary/75">
+                  Pellentesque sollicitudin malesuada lorem porta sagittis.
+                </p>
+              </div>
+
+              <div className="flex items-center gap-2 sm:gap-2.5">
+                <svg
+                  width="24"
+                  height="25"
+                  viewBox="0 0 24 25"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="w-[16px] sm:w-[24px] h-auto"
+                >
+                  <path
+                    d="M22 12.0352C22 6.51231 17.5228 2.03516 12 2.03516C6.47715 2.03516 2 6.51231 2 12.0352C2 17.558 6.47715 22.0352 12 22.0352C17.5228 22.0352 22 17.558 22 12.0352Z"
+                    stroke="#A400DD"
+                    strokeWidth="1.5"
+                  />
+                  <path
+                    d="M8 12.5352L10.5 15.0352L16 9.03516"
+                    stroke="#A400DD"
+                    strokeWidth="1.5"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                </svg>
+
+                <p className="text-xs sm:text-base tracking-[-0.12px] sm:tracking-[-0.16px] text-primary/75">
+                  Quisque id tortor quis diam porttitor euismod.
+                </p>
+              </div>
+            </div>
+
+            <button className="active:scale-95 w-full sm:w-fit py-2.5 sm:py-2 px-5 border border-dark-white bg-white rounded-lg flex justify-center items-center gap-2.5 text-xs sm:text-base font-medium tracking-[-0.16px] text-[#A400DD] duration-300">
+              View all use cases
+              <svg
+                width="13"
+                height="11"
+                viewBox="0 0 13 11"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+                className="w-[10px] sm:w-[13px] h-auto"
+              >
+                <path
+                  d="M7.71967 2.03033C7.42678 1.73744 7.42678 1.26256 7.71967 0.96967C8.01256 0.676777 8.48744 0.676777 8.78033 0.96967L12.7803 4.96967C13.0732 5.26256 13.0732 5.73744 12.7803 6.03033L8.78033 10.0303C8.48744 10.3232 8.01256 10.3232 7.71967 10.0303C7.42678 9.73744 7.42678 9.26256 7.71967 8.96967L10.4393 6.25H0.75C0.335787 6.25 0 5.91421 0 5.5C0 5.08579 0.335787 4.75 0.75 4.75H10.4393L7.71967 2.03033Z"
+                  fill="#A400DD"
+                />
+              </svg>
+            </button>
+          </div>
+        </div>
+
+        <div className="mt-10 sm:mt-20 grid grid-cols-2 lg:grid-cols-4 gap-8 sm:gap-10">
+          {infoCards.map((card, index) => (
+            <InfoCard key={index} {...card} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
---
+++ src/app/use-cases/ad-verification/ProductsSection.tsx
@@ -0,0 +1,336 @@
+import Image from "next/image";
+import product1IconImg from "@/assets/images/product-1-icon.svg";
+import product2IconImg from "@/assets/images/product-2-icon.svg";
+import product3IconImg from "@/assets/images/product-3-icon.svg";
+import product4IconImg from "@/assets/images/product-4-icon.svg";
+import product5IconImg from "@/assets/images/product-5-icon.svg";
+import PrimaryBtn from "@/components/ui/PrimaryBtn";
+
+interface IProductBigCardProps {
+  isBigger?: boolean;
+  color: string;
+  icon: JSX.Element;
+  title: string;
+  label?: string;
+  description: string;
+  price: string;
+}
+
+interface IProductCardProps {
+  color: string;
+  icon: JSX.Element;
+  title: string;
+  description: string;
+  statement1: string;
+  statement2: string;
+  price: string;
+}
+
+function ProductBigCard({
+  isBigger,
+  color,
+  icon,
+  title,
+  label,
+  description,
+  price,
+}: IProductBigCardProps) {
+  return (
+    <div
+      className={`p-8 border border-dark-white bg-off-white rounded-2xl ${
+        isBigger
+          ? "col-span-2 flex justify-between items-center gap-5"
+          : "space-y-5"
+      }`}
+    >
+      <div className={`${isBigger ? "flex items-center gap-5" : "space-y-5"}`}>
+        {icon}
+
+        <div>
+          <div className="mb-1.5 flex items-center gap-2.5">
+            <h3 className="text-2xl font-semibold tracking-[-0.48px]">
+              {title}
+            </h3>
+
+            {label && (
+              <span className="py-1 px-3.5 border border-[#00BB50] bg-[#00BB50]/10 rounded-full text-xs font-semibold text-[#00BB50]">
+                {label}
+              </span>
+            )}
+          </div>
+
+          <p className="max-w-[540px] text-base tracking-[-0.16px] text-primary/50">
+            {description}
+          </p>
+        </div>
+      </div>
+
+      <button
+        className="active:scale-95 py-2 px-5 border border-dark-white bg-white rounded-lg text-base font-medium tracking-[-0.16px] flex items-center gap-2.5 duration-300"
+        style={{
+          color,
+        }}
+      >
+        Starting from {price}
+        <svg
+          width="13"
+          height="11"
+          viewBox="0 0 13 11"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M7.71967 2.03033C7.42678 1.73744 7.42678 1.26256 7.71967 0.96967C8.01256 0.676777 8.48744 0.676777 8.78033 0.96967L12.7803 4.96967C13.0732 5.26256 13.0732 5.73744 12.7803 6.03033L8.78033 10.0303C8.48744 10.3232 8.01256 10.3232 7.71967 10.0303C7.42678 9.73744 7.42678 9.26256 7.71967 8.96967L10.4393 6.25H0.75C0.335787 6.25 0 5.91421 0 5.5C0 5.08579 0.335787 4.75 0.75 4.75H10.4393L7.71967 2.03033Z"
+            fill={color}
+          />
+        </svg>
+      </button>
+    </div>
+  );
+}
+
+function ProductCard({
+  color,
+  icon,
+  title,
+  description,
+  statement1,
+  statement2,
+  price,
+}: IProductCardProps) {
+  return (
+    <div className="p-5 border border-dark-white rounded-xl">
+      <div className="relative mb-4 flex items-center gap-2.5 sm:gap-3">
+        <span
+          className="absolute top-1/2 -left-5 -translate-y-1/2 w-[4px] h-[32px] rounded-e-full"
+          style={{ background: color }}
+        ></span>
+
+        {icon}
+
+        <h3 className="text-base sm:text-xl font-semibold tracking-[-0.16px]">
+          {title}
+        </h3>
+      </div>
+
+      <p className="mb-5 text-[11px] sm:text-base !leading-[15px] tracking-[-0.11px] sm:tracking-[-0.15px]">
+        {description}
+      </p>
+
+      <div className="mb-5 space-y-2.5">
+        <div className="flex items-center gap-2.5">
+          <svg
+            width="24"
+            height="24"
+            viewBox="0 0 38 38"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            className="w-[24px] sm:w-[32px] h-auto"
+          >
+            <rect
+              width="38"
+              height="38"
+              rx="10"
+              fill={color}
+              fillOpacity="0.1"
+            />
+            <path
+              d="M15 17L19 19M15 17V21L19 23M15 17L19 15L23 17M19 19L23 17M19 19V23M19 23L23 21V17M16.4 28C14.16 28 13.04 28 12.184 27.564C11.4314 27.1805 10.8195 26.5686 10.436 25.816C10 24.96 10 23.84 10 21.6M28 21.6C28 23.84 28 24.96 27.564 25.816C27.1805 26.5686 26.5686 27.1805 25.816 27.564C24.96 28 23.84 28 21.6 28M21.6 10C23.84 10 24.96 10 25.816 10.436C26.5686 10.8195 27.1805 11.4314 27.564 12.184C28 13.04 28 14.16 28 16.4M16.4 10C14.16 10 13.04 10 12.184 10.436C11.4314 10.8195 10.8195 11.4314 10.436 12.184C10 13.04 10 14.16 10 16.4"
+              stroke={color}
+              strokeWidth="1.5"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+          </svg>
+
+          <p className="text-[11px] sm:text-base tracking-[-0.11px] sm:tracking-[-0.15px]">
+            {statement1}
+          </p>
+        </div>
+
+        <div className="flex items-center gap-2.5">
+          <svg
+            width="24"
+            height="24"
+            viewBox="0 0 38 38"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            className="w-[24px] sm:w-[32px] h-auto"
+          >
+            <rect
+              width="38"
+              height="38"
+              rx="10"
+              fill={color}
+              fillOpacity="0.1"
+            />
+            <path
+              d="M28 18.9999H25.038C24.828 18.9999 24.722 18.9999 24.633 19.0419C24.5519 19.0808 24.4826 19.1405 24.432 19.2149C24.371 19.3029 24.34 19.4199 24.277 19.6549L22.46 26.5009C22.227 27.3759 22.111 27.8139 21.936 27.9269C21.8641 27.9747 21.7796 28.0001 21.6932 27.9997C21.6068 27.9994 21.5225 27.9733 21.451 27.9249C21.276 27.8099 21.163 27.3709 20.937 26.4949L17.064 11.5049C16.837 10.6289 16.724 10.1899 16.549 10.0749C16.4775 10.0264 16.3932 10.0004 16.3068 10C16.2204 9.99965 16.1359 10.025 16.064 10.0729C15.889 10.1859 15.773 10.6229 15.54 11.4989L13.723 18.3439C13.66 18.5799 13.629 18.6969 13.569 18.7839C13.5183 18.8588 13.4486 18.9188 13.367 18.9579C13.278 18.9999 13.173 18.9999 12.962 18.9999H10"
+              stroke={color}
+              strokeWidth="1.5"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+          </svg>
+
+          <p className="text-[11px] sm:text-base tracking-[-0.11px] sm:tracking-[-0.15px]">
+            {statement2}
+          </p>
+        </div>
+      </div>
+
+      <div className="flex justify-between items-end">
+        <div>
+          <p className="text-[9px] sm:text-sm font-medium tracking-[-0.09px] text-primary/60">
+            Starting from
+          </p>
+
+          <p className="text-sm sm:text-lg font-semibold tracking-[-0.14px]">
+            {price}
+          </p>
+        </div>
+
+        <PrimaryBtn extraSmall customClasses="!px-8 !text-xs sm:!text-base">
+          Get Started
+        </PrimaryBtn>
+      </div>
+    </div>
+  );
+}
+
+export default function ProductsSection() {
+  const productBigCards: IProductBigCardProps[] = [
+    {
+      color: "#1675FF",
+      icon: <Image src={product1IconImg} alt="" className="w-[50px]" />,
+      title: "Residential Proxies",
+      label: "25% off",
+      description:
+        "Interdum et malesuada fames ac ante ipsum primis in faucibus. Etiam quis nibh non nisl sagittis malesuada eget nec velit.",
+      price: "$2.99/GB",
+    },
+    {
+      color: "#12A250",
+      icon: <Image src={product2IconImg} alt="" className="w-[50px]" />,
+      title: "Datacenter Proxies",
+      description:
+        "Interdum et malesuada fames ac ante ipsum primis in faucibus. Etiam quis nibh non nisl sagittis malesuada eget nec velit.",
+      price: "$1.99/Day",
+    },
+    {
+      color: "#EA7635",
+      icon: <Image src={product3IconImg} alt="" className="w-[50px]" />,
+      title: "ISP Proxies",
+      description:
+        "Interdum et malesuada fames ac ante ipsum primis in faucibus. Etiam quis nibh non nisl sagittis malesuada eget nec velit.",
+      price: "$5.99/GB",
+    },
+    {
+      color: "#A400DD",
+      icon: <Image src={product4IconImg} alt="" className="w-[50px]" />,
+      title: "IPv6 Proxies",
+      description:
+        "Interdum et malesuada fames ac ante ipsum primis in faucibus. Etiam quis nibh non nisl sagittis malesuada eget nec velit.",
+      price: "$9.99/GB",
+    },
+    {
+      isBigger: true,
+      color: "#00C1D1",
+      icon: <Image src={product5IconImg} alt="" className="w-[50px]" />,
+      title: "Mobile Proxies",
+      label: "Newly launched",
+      description:
+        "Interdum et malesuada fames ac ante ipsum primis in faucibus. Etiam quis nibh non nisl sagittis malesuada eget nec velit.",
+      price: "$5.99/GB",
+    },
+  ];
+
+  const productCards: IProductCardProps[] = [
+    {
+      color: "#1675FF",
+      icon: (
+        <Image src={product1IconImg} alt="" className="w-[35px] sm:w-[45px]" />
+      ),
+      title: "Residential Proxies",
+      description:
+        "Sed varius justo ac sem luctus lobortis. Nunc porta leo nec pretium laoreet.",
+      statement1: "Statement 01 about the proxies features",
+      statement2: "Statement 02 about the proxies features",
+      price: "$2/GB",
+    },
+    {
+      color: "#12A250",
+      icon: (
+        <Image src={product2IconImg} alt="" className="w-[35px] sm:w-[45px]" />
+      ),
+      title: "Residential Proxies",
+      description:
+        "Sed varius justo ac sem luctus lobortis. Nunc porta leo nec pretium laoreet.",
+      statement1: "Statement 01 about the proxies features",
+      statement2: "Statement 02 about the proxies features",
+      price: "$9/Day",
+    },
+    {
+      color: "#EA7635",
+      icon: (
+        <Image src={product3IconImg} alt="" className="w-[35px] sm:w-[45px]" />
+      ),
+      title: "ISP Proxies",
+      description:
+        "Sed varius justo ac sem luctus lobortis. Nunc porta leo nec pretium laoreet.",
+      statement1: "Statement 01 about the proxies features",
+      statement2: "Statement 02 about the proxies features",
+      price: "$4.99",
+    },
+    {
+      color: "#A400DD",
+      icon: (
+        <Image src={product4IconImg} alt="" className="w-[35px] sm:w-[45px]" />
+      ),
+      title: "IPv6 Proxies",
+      description:
+        "Sed varius justo ac sem luctus lobortis. Nunc porta leo nec pretium laoreet.",
+      statement1: "Statement 01 about the proxies features",
+      statement2: "Statement 02 about the proxies features",
+      price: "$4.99",
+    },
+    {
+      color: "#00C1D1",
+      icon: (
+        <Image src={product5IconImg} alt="" className="w-[35px] sm:w-[45px]" />
+      ),
+      title: "Mobile Proxies",
+      description:
+        "Sed varius justo ac sem luctus lobortis. Nunc porta leo nec pretium laoreet.",
+      statement1: "Statement 01 about the proxies features",
+      statement2: "Statement 02 about the proxies features",
+      price: "$2/GB",
+    },
+  ];
+
+  return (
+    <section className="pt-20 sm:pt-44">
+      <div className="mx-auto container max-w-[1320px] px-5">
+        <p className="mb-3 text-[11px] sm:text-sm font-semibold tracking-[-0.11px] sm:tracking-[-0.14px] uppercase text-center text-accent">
+          Products
+        </p>
+
+        <h2 className="mb-8 sm:mb-14 text-[26px] sm:text-[42px] font-semibold leading-[30px] sm:leading-[46px] tracking-[-0.84px] text-center">
+          Proxy products to power <br /> your web scraping projects
+        </h2>
+
+        <div className="hidden lg:grid grid-cols-2 gap-4">
+          {productBigCards.map((productBigCard, index) => (
+            <ProductBigCard key={index} {...productBigCard} />
+          ))}
+        </div>
+
+        <div className="block lg:hidden space-y-4">
+          {productCards.map((productCard, index) => (
+            <ProductCard key={index} {...productCard} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
