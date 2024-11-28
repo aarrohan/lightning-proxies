c7992c850f81e872242fb1336f2fdff1caefac48
Abdullah Al Reza Rohan <developer.aarrohan@gmail.com>
2024-11-29 03:02:10 +0600

    feat(ui): add paginations in articles section of blog page

--- src/app/blog/ArticlesSection.tsx
+++ src/app/blog/ArticlesSection.tsx
@@ -10,6 +10,7 @@ import ArticleCard, {
 
 export default function ArticlesSection() {
   const [tabIndex, setTabIndex] = useState<number>(0);
+  const [activePage, setActivePage] = useState<number>(1);
 
   const articles: IArticleCard[] = [
     {
@@ -85,6 +86,93 @@ export default function ArticlesSection() {
           {articles.map((article, index) => {
             return <ArticleCard key={index} {...article} />;
           })}
+        </div>
+
+        <div className="mt-14 flex justify-center gap-2">
+          <button
+            onClick={() => {
+              if (activePage > 1) {
+                setActivePage(activePage - 1);
+              }
+            }}
+            className={`w-[30px] sm:w-[44px] aspect-square border border-dark-white bg-white rounded-md flex justify-center items-center text-xs sm:text-base font-medium ${
+              activePage <= 1 ? "opacity-50" : ""
+            } duration-200`}
+          >
+            <svg
+              width="7"
+              height="10"
+              viewBox="0 0 7 10"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fillRule="evenodd"
+                clipRule="evenodd"
+                d="M6.03033 0.46967C5.73744 0.176777 5.26256 0.176777 4.96967 0.46967L0.96967 4.46967C0.676777 4.76256 0.676777 5.23744 0.96967 5.53033L4.96967 9.53033C5.26256 9.82322 5.73744 9.82322 6.03033 9.53033C6.32322 9.23744 6.32322 8.76256 6.03033 8.46967L2.56066 5L6.03033 1.53033C6.32322 1.23744 6.32322 0.762563 6.03033 0.46967Z"
+                fill="#1D2026"
+              />
+            </svg>
+          </button>
+
+          <button
+            onClick={() => setActivePage(1)}
+            className={`w-[30px] sm:w-[44px] aspect-square border ${
+              activePage === 1
+                ? "border-dark-white bg-white"
+                : "border-transparent"
+            } rounded-md flex justify-center items-center text-xs sm:text-base font-medium duration-200`}
+          >
+            1
+          </button>
+
+          <button
+            onClick={() => setActivePage(2)}
+            className={`w-[30px] sm:w-[44px] aspect-square border ${
+              activePage === 2
+                ? "border-dark-white bg-white"
+                : "border-transparent"
+            } rounded-md flex justify-center items-center text-xs sm:text-base font-medium duration-200`}
+          >
+            2
+          </button>
+
+          <button
+            onClick={() => setActivePage(3)}
+            className={`w-[30px] sm:w-[44px] aspect-square border ${
+              activePage === 3
+                ? "border-dark-white bg-white"
+                : "border-transparent"
+            } rounded-md flex justify-center items-center text-xs sm:text-base font-medium duration-200`}
+          >
+            3
+          </button>
+
+          <button
+            onClick={() => {
+              if (activePage < 3) {
+                setActivePage(activePage + 1);
+              }
+            }}
+            className={`w-[30px] sm:w-[44px] aspect-square border border-dark-white bg-white rounded-md flex justify-center items-center text-xs sm:text-base font-medium ${
+              activePage >= 3 ? "opacity-50" : ""
+            } duration-200`}
+          >
+            <svg
+              width="7"
+              height="10"
+              viewBox="0 0 7 10"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fillRule="evenodd"
+                clipRule="evenodd"
+                d="M0.96967 0.46967C1.26256 0.176777 1.73744 0.176777 2.03033 0.46967L6.03033 4.46967C6.32322 4.76256 6.32322 5.23744 6.03033 5.53033L2.03033 9.53033C1.73744 9.82322 1.26256 9.82322 0.96967 9.53033C0.676777 9.23744 0.676777 8.76256 0.96967 8.46967L4.43934 5L0.96967 1.53033C0.676777 1.23744 0.676777 0.762563 0.96967 0.46967Z"
+                fill="#1D2026"
+              />
+            </svg>
+          </button>
         </div>
       </div>
     </section>
