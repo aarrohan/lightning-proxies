91eb3e596c535a4384cd4a9da97bdf8487d32751
Abdullah Al Reza Rohan <developer.aarrohan@gmail.com>
2024-12-01 14:35:16 +0600

    feat(ui): add affiliate program page

---
+++ src/app/affiliate-program/page.tsx
@@ -0,0 +1,11 @@
+import HeroSection from "./HeroSection";
+import FollowUpSection from "@/app/(landing)/FollowUpSection";
+
+export default function Affiliate() {
+  return (
+    <main>
+      <HeroSection />
+      <FollowUpSection />
+    </main>
+  );
+}
