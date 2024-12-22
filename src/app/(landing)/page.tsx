b55b5cce621b003e6a62c588cbe70e1fd29d132e
Abdullah Al Reza Rohan <developer.aarrohan@gmail.com>
2024-12-23 01:47:38 +0600

    feat(ui): work on changes

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -1,3 +1,4 @@
+import type { Metadata } from "next";
 import HeroSection from "./HeroSection";
 import ClientsSection from "./ClientsSection";
 import ProductsSection from "./ProductsSection";
@@ -12,6 +13,22 @@ import ExtensionSection from "./ExtensionSection";
 import BlogSection from "./BlogSection";
 import UpcomingSection from "./UpcomingSection";
 import FollowUpSection from "./FollowUpSection";
+
+export const metadata: Metadata = {
+  title:
+    "LightningProxies - Simplified Proxy Solutions for Web Data Extraction",
+  description:
+    "Access 20M+ premium Residential, Datacenter, and IPv6 Proxies. Designed for seamless web data extraction, scraping, and monitoring, we provide reliable proxy solutions tailored to your business needs.",
+  openGraph: {
+    type: "website",
+    url: process.env.NEXT_PUBLIC_APP_URL,
+    title:
+      "LightningProxies - Simplified Proxy Solutions for Web Data Extraction",
+    description:
+      "Access 20M+ premium Residential, Datacenter, and IPv6 Proxies. Designed for seamless web data extraction, scraping, and monitoring, we provide reliable proxy solutions tailored to your business needs.",
+    images: "https://i.imgur.com/s2kQsiw.png",
+  },
+};
 
 export default function Home() {
   return (
