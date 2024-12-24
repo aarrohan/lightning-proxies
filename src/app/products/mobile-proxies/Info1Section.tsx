5060f0fc4dcbf8a590119c880ce864ace382d389
Abdullah Al Reza Rohan <developer.aarrohan@gmail.com>
2024-12-25 05:33:12 +0600

    feat(ui): work on change

--- src/app/products/mobile-proxies/Info1Section.tsx
+++ src/app/products/mobile-proxies/Info1Section.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import infoImg from "@/assets/images/product-5-info-1.png";
 import SecondaryBtn from "@/components/ui/SecondaryBtn";
+import Link from "next/link";
 
 export default function Info1Section() {
   return (
@@ -115,21 +116,23 @@ export default function Info1Section() {
           </div>
 
           <div className="-ml-2">
-            <SecondaryBtn customClasses="sm:!py-2 !px-8 !border-2 !border-dark-white sm:!text-base sm:!tracking-[-0.16px]">
-              Get Started{" "}
-              <svg
-                width="14"
-                height="11"
-                viewBox="0 0 14 11"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M8.21967 2.03033C7.92678 1.73744 7.92678 1.26256 8.21967 0.96967C8.51256 0.676777 8.98744 0.676777 9.28033 0.96967L13.2803 4.96967C13.5732 5.26256 13.5732 5.73744 13.2803 6.03033L9.28033 10.0303C8.98744 10.3232 8.51256 10.3232 8.21967 10.0303C7.92678 9.73744 7.92678 9.26256 8.21967 8.96967L10.9393 6.25H1.25C0.835787 6.25 0.5 5.91421 0.5 5.5C0.5 5.08579 0.835787 4.75 1.25 4.75H10.9393L8.21967 2.03033Z"
-                  className="fill-accent"
-                />
-              </svg>
-            </SecondaryBtn>
+            <Link href={`${process.env.NEXT_PUBLIC_AUTH_URL}/signup`}>
+              <SecondaryBtn customClasses="sm:!py-2 !px-8 !border-2 !border-dark-white sm:!text-base sm:!tracking-[-0.16px]">
+                Get Started{" "}
+                <svg
+                  width="14"
+                  height="11"
+                  viewBox="0 0 14 11"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M8.21967 2.03033C7.92678 1.73744 7.92678 1.26256 8.21967 0.96967C8.51256 0.676777 8.98744 0.676777 9.28033 0.96967L13.2803 4.96967C13.5732 5.26256 13.5732 5.73744 13.2803 6.03033L9.28033 10.0303C8.98744 10.3232 8.51256 10.3232 8.21967 10.0303C7.92678 9.73744 7.92678 9.26256 8.21967 8.96967L10.9393 6.25H1.25C0.835787 6.25 0.5 5.91421 0.5 5.5C0.5 5.08579 0.835787 4.75 1.25 4.75H10.9393L8.21967 2.03033Z"
+                    className="fill-accent"
+                  />
+                </svg>
+              </SecondaryBtn>
+            </Link>
           </div>
         </div>
       </div>
